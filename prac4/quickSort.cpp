0f5209a60a2194bd80ba75bb2feeb0ac00292a67
Sameer Chore <sameerchore5@gmail.com>
2026-08-11 18:01:06 +0530

    prac 4 done of deterministic quick sort

--- prac4/quickSort.cpp
+++ prac4/quickSort.cpp
@@ -1,29 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high];   // Deterministic pivot
-
-    int i = low - 1;
-
-    for (int j = low; j < high; j++) {
-        if (arr[j] <= pivot) {
-            i++;
-            swap(arr[i], arr[j]);
-        }
-    }
-
-    swap(arr[i + 1], arr[high]);
-
-    return i + 1;
-}
-
 void quickSort(int arr[], int low, int high) {
     if (low < high) {
-        int p = partition(arr, low, high);
+        int pivot = arr[high];
+        int i = low - 1;
 
-        quickSort(arr, low, p - 1);
-        quickSort(arr, p + 1, high);
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr[i], arr[j]);
+            }
+        }
+        swap(arr[i + 1], arr[high]);
+        int pi = i + 1;
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
     }
 }
 
