18c067ae0001e643dd7dee043b186d234682a665
Sameer Chore <sameerchore5@gmail.com>
2026-09-01 16:44:05 +0530

    linear min max appr

---
+++ prac6/appr2minMax.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <climits>
+#include <iostream>
+using namespace std;
+
+vector<int> findMinMax(vector<int>& arr) {
+    int n = arr.size();
+    int mini = INT_MAX, maxi = INT_MIN;
+    
+    for (int i = 0; i < n; i++) { 
+        if (arr[i] < mini) mini = arr[i];
+        if (arr[i] > maxi) maxi = arr[i];
+    }
+    
+    return {mini, maxi};
+}
+
+int main() {
+    vector<int> arr = {3, 5, 4, 1, 9};
+    vector<int> result = findMinMax(arr);
+    cout << result[0] << " " << result[1] << endl;
+    return 0;
+}
